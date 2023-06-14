44e19501c2ee3068b04b37c5ecb0e9d1bd3c533e
Jerricke <eejerrickw@gmail.com>
2023-06-14 12:19:47 -0600

    update

---
+++ 02_DOM_Manipulation/src/review.js
@@ -0,0 +1,23 @@
+//Objects
+let flatironSchool = {
+    west: {
+        location: "",
+        address: "2228 Blake St",
+        hours: "7am-6:15pm",
+        courses: ["Software Engineering", "Data Science", "CyberSecurity", "Product Design"]
+    }
+}
+
+
+//modify the object to have the correct location of denver
+
+//modify the object to have the address 2228 Blake St
+
+//modify the object to have the hours from 7am = 6:15pm
+
+//add a new course to the west object
+
+//add a new object to flatiron school, the key of this object should be east, the object should have the same keys of location, address, hours, and course
+//The values should then be "New York", "11 Broadway 2nd floor, New York, NY 10004", "9am - 6pm", courses should be "Software Engineering", "Data Science", "CyberSecurity", "Product Design"
+
+
